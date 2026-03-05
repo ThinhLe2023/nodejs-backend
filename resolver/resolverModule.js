40e7fa0d9043e1970d73ed241911cec769d46b56
Quoc Le <lequocthinh2010@gmail.com>
2026-03-05 13:24:07 -0600

    fix format of resolve

--- resolver/resolverModule.js
+++ resolver/resolverModule.js
@@ -1,13 +1,13 @@
 import { mergeResolvers } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
-import {join} from 'path';
+import { join } from 'path';
 
 const resolversArray = loadFilesSync(
-     join(import.meta.dirname,'./**/*.resolver.cjs'),
+     join(import.meta.dirname, './**/*.resolver.cjs'),
      { extensions: ['cjs'] },
 );
 
-console.log(join(import.meta.dirname,'./**/*.resolver.cjs'));
+console.log(join(import.meta.dirname, './**/*.resolver.cjs'));
 console.log('Loaded Resolvers:', resolversArray);
 
 // Merge your resolvers (imported from your modules)
